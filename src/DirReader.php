f2519a32ceba5e7afdd20069e8c23afc7ba21abc
Praveen Kumar <praveen.42000@gmail.com>
2022-05-23 02:31:44 +0530

    changed to oops

---
+++ src/DirReader.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Techunico\DirReader;
+
+class reader{
+
+  public function dirreader($directory, array $excludefiles = ['.', '..']) {
+
+      $files = [];
+
+      if (!is_dir($directory)) {
+          return null;
+      }
+
+      if (!($filedir = opendir($directory))) {
+          return null;
+      }
+
+      while (($file = readdir($filedir)) !== false) {
+
+          if (in_array($file, $excludefiles)) {
+              continue;
+          }
+
+          $files[] = $directory . '/' . $file;
+      }
+
+      closedir($filedir);
+      return $files;
+  }
+}
